93372262f8bd625829d0a0fd56a8c2ccf6c9eccc
drewstephensdesigns <usafcrewchief06@yahoo.com>
2024-06-28 14:26:31 -0500

    Added WebView, updated Settings and Consts files

--- AMXSFamilyZone/AMXSFamilyZone/Utils/Consts.swift
+++ AMXSFamilyZone/AMXSFamilyZone/Utils/Consts.swift
@@ -13,7 +13,10 @@ struct Consts {
     static let POST_NODE = "Post"
     static let REPORTS_NODE = "Reports"
     static let COMMENTS_NODE = "Comments"
+    static let IMAGES_NODE = "Images"
     static let FEATURED_URL = "https://drewstephensdesigns.github.io/AMXSFamilyZone/data/"
+    
+    static let SOURCE_CODE = "https://github.com/drewstephensdesigns/AMXSFamilyZone-iOS"
 
     static func saveToClipboard(text: String) {
         let pasteboard = UIPasteboard.general
