d2c0b13d11b491ac512650919a6631180e249ed3
drewstephensdesigns <usafcrewchief06@yahoo.com>
2024-06-28 23:01:21 -0500

    SettingsView, WebView and Consts files updates

--- AMXSFamilyZone/AMXSFamilyZone/Utils/Consts.swift
+++ AMXSFamilyZone/AMXSFamilyZone/Utils/Consts.swift
@@ -9,15 +9,21 @@ import SwiftUI
 import UIKit
 
 struct Consts {
+    // Firebase Database
     static let USER_NODE = "Users"
     static let POST_NODE = "Post"
     static let REPORTS_NODE = "Reports"
     static let COMMENTS_NODE = "Comments"
-    static let IMAGES_NODE = "Images"
-    static let FEATURED_URL = "https://drewstephensdesigns.github.io/AMXSFamilyZone/data/"
     
+    // Firebase Storage
+    static let IMAGES_NODE = "Images"
+    
+    // URLS
+    static let FEATURED_URL = "https://drewstephensdesigns.github.io/AMXSFamilyZone/data/"
     static let SOURCE_CODE = "https://github.com/drewstephensdesigns/AMXSFamilyZone-iOS"
 
+    // Static function for saving to clipboard
+    // function not yet used
     static func saveToClipboard(text: String) {
         let pasteboard = UIPasteboard.general
         pasteboard.string = text
